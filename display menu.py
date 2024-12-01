1c33f8d5fa4403e5554a09feec6d84007386096e
Mohammed Madani Ibrahim Ali Khan <165456579+Md-Madani@users.noreply.github.com>
2024-12-01 20:50:57 +0530

    Create display menu.py

---
+++ display menu.py
@@ -0,0 +1,9 @@
+# Function to display the menu
+def display_menu():
+    print("\n--- To-Do List ---")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Mark Task as Completed")
+    print("4. Delete Task")
+    print("5. Save & Exit")
+    print("6. View Statistics")
